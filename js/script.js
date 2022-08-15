21fd8eafbf92ff66f0d1c6ebbff3a35ae3ca49a1
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-15 13:30:36 +0200

    Basic operator function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 import * as mathFunc from "./math_functions.js";
 
-console.log(mathFunc.add(4, 5));
-console.log(mathFunc.add(4, 5, 5, 6));
+// TEST DATA
+// console.log(mathFunc.add(4, 5));
+// console.log(mathFunc.add(4, 5, 5, 6));
 
-console.log(mathFunc.subtract(8, 5));
-console.log(mathFunc.subtract(10, 5, 3, 8));
+// console.log(mathFunc.subtract(8, 5));
+// console.log(mathFunc.subtract(10, 5, 3, 8));
 
-console.log(mathFunc.multiply(4, 5));
-console.log(mathFunc.multiply(4, 5, 5, 6));
+// console.log(mathFunc.multiply(4, 5));
+// console.log(mathFunc.multiply(4, 5, 5, 6));
 
-console.log(mathFunc.division(10, 5));
-console.log(mathFunc.division(20, 5, 2, 5));
+// console.log(mathFunc.division(10, 5));
+// console.log(mathFunc.division(20, 5, 2, 5));
+
+function operator(num1, num2, func) {
+  if (func === "+") {
+    return mathFunc.add(num1, num2);
+  } else if (func === "-") {
+    return mathFunc.subtract(num1, num2);
+  } else if (func === "*") {
+    return mathFunc.multiply(num1, num2);
+  } else if (func === "/") {
+    return mathFunc.division(num1, num2);
+  }
+}
+
+console.log(operator(7, 3, "+"));
+console.log(operator(7, 3, "-"));
+console.log(operator(7, 3, "*"));
+console.log(operator(7, 3, "/"));
