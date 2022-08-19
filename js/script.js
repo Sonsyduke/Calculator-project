a9ff6f89c29ce79e0d7cc7f4038e8fec5559d8a4
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-19 10:16:46 +0200

    Starting from scratch, finished the delete and clear functions

--- js/script.js
+++ js/script.js
@@ -1,47 +1,48 @@
 import * as mathFunc from "./math_functions.js";
 
-class Calculator {
-  constructor(displayTop, displayBottom) {
-    this.displayTop = displayTop;
-    this.displayBottom = displayBottom;
-    this.clear();
-  }
-
-  clear() {
-    this.currentOperand = "";
-    this.previousOperand = "";
-    this.operation = undefined;
-  }
-  delete() {}
-  appendNumber(number) {
-    if (number === "." && this.currentOperand.includes(".")) return;
-    this.currentOperand = this.currentOperand.toString() + number.toString();
-  }
-  chooseOperation(operation) {}
-  compute() {}
-  updateDisplay() {
-    this.displayBottom.innerText = this.currentOperand;
-  }
-}
-
 // Getting all the buttons
 let displayTop = document.querySelector(".top-section");
 let displayBottom = document.querySelector(".buttom-section");
-// const buttons = [...document.querySelectorAll(".button")];
 const numericButtons = [...document.querySelectorAll(".numeric")];
 const operationButtons = [...document.querySelectorAll(".operation")];
 const equalsButton = document.querySelector("#equal");
-const deleteButton = document.querySelector("#clear");
-const clearButton = document.querySelector("#delete");
+const deleteButton = document.querySelector("#delete");
+const clearButton = document.querySelector("#clear");
 
-const calculator = new Calculator(displayTop, displayBottom);
+let currentOperand = "";
+let previousOperand = "";
+let operation = undefined;
 
-numericButtons.map((numButton) => {
-  numButton.addEventListener("click", () => {
-    calculator.appendNumber(numButton.innerText);
-    calculator.updateDisplay();
+numericButtons.map((button) => {
+  button.addEventListener("click", (e) => {
+    currentOperand = e.target.innerText;
+    displayBottom.innerText += currentOperand;
+    currentOperand = displayBottom.innerText;
+    console.log(currentOperand);
   });
 });
+
+clearButton.addEventListener("click", () => {
+  clear();
+});
+deleteButton.addEventListener("click", () => {
+  deleteNum();
+});
+
+// ===================================
+// FUNCTIONS
+function clear() {
+  currentOperand = "";
+  previousOperand = "";
+  operation = undefined;
+  displayBottom.innerText = currentOperand;
+  displayTop.innerText = previousOperand;
+}
+
+function deleteNum() {
+  currentOperand = currentOperand.slice(0, -1);
+  displayBottom.innerText = currentOperand;
+}
 
 function operator(func, ...numbers) {
   if (func === "+") {
@@ -54,8 +55,3 @@ function operator(func, ...numbers) {
     return mathFunc.division(...numbers);
   }
 }
-
-// console.log(operator("+", 7, 3, 12, 56));
-// console.log(operator("-", 15, 6, 4));
-// console.log(operator("*", 5, 6, 6));
-// console.log(operator("/", 5, 6, 6));
